bf12b92f9fea59b0f53552ede8df5c3e06841105
Emma Frantz <ecfrantz@mymail.mines.edu>
2019-02-20 19:07:11 -0700

    added to makeInputs

--- makeInputs.cpp
+++ makeInputs.cpp
@@ -1,2 +1,25 @@
+using namespace std;
+#include <vector> 
+#include <utility>
+#include <math.h>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <istream>
+
 int main(){
+    vector<int> inputs;
+    int num = 1000;
+    srand (time(NULL));
+    int n = rand() % 10; 
+    inputs.push_back(n);
+    for(int i=0; i<num; i++){
+        int m = rand() % n + 1000000; 
+        inputs.push_back(m);
+        n = m;
+    }
+    ofstream outfile("inputs.txt");
+    for(int i=0; i<num; i++){
+        outfile << inputs[i] << endl;
+    }
 }
