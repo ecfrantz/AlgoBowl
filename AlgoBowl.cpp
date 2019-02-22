ba6842e112637ad0765f14e2be177305ede638db
Emma Frantz <ecfrantz@mymail.mines.edu>
2019-02-21 20:23:33 -0700

    algobowl edit

--- AlgoBowl.cpp
+++ AlgoBowl.cpp
@@ -1,56 +1,44 @@
  using namespace std;
- 
-#include <string>
- 
-#include <iostream>
- 
-#include <fstream>
- 
-#include <istream>
- 
-#include <vector> 
- 
-#include <utility>
- 
-#include <math.h>
- 
-#include <iomanip>
+ #include <string>
+ #include <iostream>
+ #include <fstream>
+ #include <istream>
+ #include <vector> 
+ #include <utility>
+ #include <math.h>
+ #include <iomanip>
  
  
- 
-
-int main(){
+ int main(){
+        vector<int> inputs;
         
-	vector<int> inputs;
+        string fileName;
+        cout << "Please enter a file name: ";
+        cin >> fileName;
+        ifstream inFile(fileName);
+        if(inFile.fail()){ // Check for failures opening
+           cerr << "Error opening input file";
+           exit(1);
+        }
         
-        
-	string fileName;
-        
-	cout << "Please enter a file name: ";
-        
-	cin >> fileName;
-        
-	ifstream inFile(fileName);
-        
-	if(inFile.fail()){ // Check for failures opening
-           
-		cerr << "Error opening input file";
-           
-		exit(1);
-        
-	}
+        int num;
+        while(inFile.eof() == false){
+           inFile >> num;
+           inputs.push_back(num);
+        }
         
         
-
-	int num;
+        vector<vector<int> additions> groups;
+        vector<int> created;
         
-	
-	while(inFile.eof() == false){
-           
-		inFile >> num;
-           
-		inputs.push_back(num);
-        
-	}
- 
-}
+        while(created != inputs){
+         for(int i=0; i < inputs; i++){
+          for(int j=i; j < inputs; j++){
+           int sum = inputs[i] + inputs[j];
+           groups[i].push_back(sum);
+           groups[i].push_back(inputs[i]);
+           groups[i].push_back(inputs[i]);
+          }
+         }
+        }
+ }
