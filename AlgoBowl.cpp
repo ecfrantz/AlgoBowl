53cccd082e3f1c4bfd79b88e11c7b556733a9cba
Matthew Miller <matthewm8m@gmail.com>
2019-02-22 08:16:10 -0700

    abstract load input into function

--- AlgoBowl.cpp
+++ AlgoBowl.cpp
@@ -1,44 +1,67 @@
- using namespace std;
- #include <string>
- #include <iostream>
- #include <fstream>
- #include <istream>
- #include <vector> 
- #include <utility>
- #include <math.h>
- #include <iomanip>
- 
- 
- int main(){
-        vector<int> inputs;
-        
-        string fileName;
-        cout << "Please enter a file name: ";
-        cin >> fileName;
-        ifstream inFile(fileName);
-        if(inFile.fail()){ // Check for failures opening
-           cerr << "Error opening input file";
-           exit(1);
-        }
-        
-        int num;
-        while(inFile.eof() == false){
-           inFile >> num;
-           inputs.push_back(num);
-        }
-        
-        
-        vector<vector<int> additions> groups;
-        vector<int> created;
-        
-        while(created != inputs){
-         for(int i=0; i < inputs; i++){
-          for(int j=i; j < inputs; j++){
-           int sum = inputs[i] + inputs[j];
-           groups[i].push_back(sum);
-           groups[i].push_back(inputs[i]);
-           groups[i].push_back(inputs[i]);
-          }
+using namespace std;
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <iomanip>
+
+/// Loads an input file by a specified path
+/// string:path - path to file input
+vector<int> loadInput(string path)
+{
+   // Create input file stream
+   ifstream inFile(path);
+
+   // Check for failure during file loading
+   if (inFile.fail())
+   {
+      cerr << "Error opening input file.";
+      exit(1);
+   }
+
+   // Initialize integers
+   vector<int> numbers;
+   int number;
+
+   // Ignore first line because it only contains the number of numbers
+   inFile >> number;
+   // Read all numbers into storage
+   while (inFile.eof() == false)
+   {
+      inFile >> number;
+      numbers.push_back(number);
+   }
+
+   return numbers;
+}
+
+int main()
+{
+   // Get input file name from user.
+   string inFileName;
+   cout << "Please enter a file name: ";
+   cin >> inFileName;
+
+   // Load inputs from file.
+   vector<int> inputs = loadInput(inFileName);
+
+   vector<vector<int> additions> groups;
+   vector<int> created;
+
+   while (created != inputs)
+   {
+      for (int i = 0; i < inputs; i++)
+      {
+         for (int j = i; j < inputs; j++)
+         {
+            int sum = inputs[i] + inputs[j];
+            groups[i].push_back(sum);
+            groups[i].push_back(inputs[i]);
+            groups[i].push_back(inputs[i]);
          }
-        }
- }
+      }
+   }
+}
